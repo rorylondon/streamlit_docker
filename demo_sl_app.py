20d11331e7a09e1c8d6825fef9236e8a8d91e0d6
Rory London <rory@rorys-mbp.home>
2023-05-15 13:35:30 +0100

    initial commit

---
+++ demo_sl_app.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+
+"""
+# Welcome to Streamlit!
+
+Edit `/demo_sl_app.py` to customize this app to your heart's desire :heart:
+
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
+
+
+st.write('hello world')
